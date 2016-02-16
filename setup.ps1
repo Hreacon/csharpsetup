3aa43b1176429467632925ecc1a907e06c6ddfaa
Nicholas Jensen <nicholasjensenhay@gmail.com>
2016-02-16 08:16:37 -0800

    Working ish script

---
+++ setup.ps1
@@ -0,0 +1,95 @@
+clear
+Write-Host "Please enter a project name"
+$project = Read-Host
+Write-Host "Please enter pairs name"
+$pair2 = Read-Host
+$pair1 = "Nicholas Jensen"
+
+# Make directory structure
+cd ..
+mkdir $project
+cd $project
+mkdir Content
+mkdir Modules
+mkdir Views
+mkdir Objects
+mkdir Tests
+
+# Copy unchanging files
+copy ..\csharpsetup\cp\main\* .
+copy ..\csharpsetup\cp\Views\* .\Views\
+copy ..\csharpsetup\cp\Content\css\* .\Content\css\
+copy ..\csharpsetup\cp\Content\js\* .\Content\js\
+
+# write files that need the project name in them
+
+# HomeModule.cs
+$file = "HomeModule.cs"
+cd Modules
+"using Nancy;" | Add-Content $file
+"using "+$project + "NS.Objects;" | Add-Content $file
+"using System.Collections.Generic;" | Add-Content $file
+
+"namespace "+$project + "NS" | Add-Content $file
+"{" | Add-Content $file
+"  public class HomeModule : NancyModule" | Add-Content $file
+"  {" | Add-Content $file
+"    public HomeModule()" | Add-Content $file
+"    {" | Add-Content $file
+"      Get["/"] = _ => {" | Add-Content $file
+'        return View["header.cshtml"];' | Add-Content $file
+"      };" | Add-Content $file
+"    }" | Add-Content $file
+"  }" | Add-Content $file
+"}" | Add-Content $file
+cd ..
+
+
+# Object.cs
+cd Objects
+$file = $project + ".cs"
+"using System.Collections.Generic;" | Add-Content $file
+"using System;" | Add-Content $file
+
+"namespace "+$project + "NS.Objects" | Add-Content $file
+"{" | Add-Content $file
+"  public class "+$project + "" | Add-Content $file
+"  {" | Add-Content $file
+"    public "+$project + "()" | Add-Content $file
+"    {" | Add-Content $file
+"    }" | Add-Content $file
+"  } // end class" | Add-Content $file
+"} // end namespace" | Add-Content $file
+cd ..
+
+cd Tests
+# ObjectTest.cs
+$file = $project + "Test.cs"
+"using Xunit;" | Add-Content $file
+"using "+$project+"NS.Objects;" | Add-Content $file
+
+"namespace "+$project+"NS" | Add-Content $file
+"{" | Add-Content $file
+"  public class "+$project+"Test" | Add-Content $file
+"  {" | Add-Content $file
+"    /* EXAMPLE" | Add-Content $file
+"    // Have a queen object that knows what coordinants its at" | Add-Content $file
+"    [Fact]" | Add-Content $file
+"    public void QueenAttack_ForCoordinants_SeeCoordinants()" | Add-Content $file
+"    {" | Add-Content $file
+"      QueenAttack queen = new QueenAttack(8, 2);" | Add-Content $file
+"      Assert.Equal(8, queen.GetX());" | Add-Content $file
+"      Assert.Equal(2, queen.GetY());" | Add-Content $file
+"    }" | Add-Content $file
+"    /**/" | Add-Content $file
+"  }" | Add-Content $file
+"}" | Add-Content $file
+cd ..
+
+git init
+git --config user.name $pair1 + " and " + $pair2
+git --config user.email "student@epicodus.com"
+git add .
+git commit -m "Initial Commit"
+
+atom .
